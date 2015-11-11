60c1ce8fa59fe11d4f5ea9a22f15e2f4cf17f831
Lars Næsbye Christensen <lars@naesbye.dk>
2015-11-11 23:02:59 +0100

    First part of moving the stuff from theme to filecompress app

---
+++ js/fileactions.js
@@ -0,0 +1,64 @@
+
+/*
+ * Copyright (c) 2015, written by Lars Næsbye Christensen, DeIC.
+ *
+ * This file is licensed under the Affero General Public License version 3
+ * or later.
+ *
+ *
+ * This file contains calls to register the MIME types for decompression
+ * and the extraction call itself. Moved over from DeiC OC7 theme. 
+ * Not functional yet (11/11/15)
+ */
+ 
+(function() {
+
+
+        /**
+         * Extract Ajax call to files_compress/ajax/extract.php
+         * @param parent filename
+         * @param parent filename
+         */
+        extract: function(filename, context) {
+            var dir = context.dir || context.fileList.getCurrentDirectory();
+            $.ajax(OC.linkTo('files_compress', 'ajax/extract.php'), {
+                type: 'POST',
+                data: {
+                    filename: filename,
+                    dir: dir
+                },
+                dataType: 'json',
+                success: function(s) {
+                    if (s['status'] == "success") {
+                        alert('Extraction was successful.');
+                    } else {
+                        alert('Could not extract.');
+                    }
+                },
+                error: function(s) {
+                    alert('Error');
+                }
+            });
+        }
+
+
+    }
+
+);
+
+this.register('application/zip', 'Extract', OC.PERMISSION_READ, '',
+    function(filename, context) {
+        context.fileActions.extract(filename, context)
+    });
+this.register('application/x-gzip', 'Extract', OC.PERMISSION_READ, '',
+    function(filename, context) {
+        context.fileActions.extract(filename, context)
+    });
+this.register('application/x-rar-compressed', 'Extract', OC.PERMISSION_READ, '',
+    function(filename, context) {
+        context.fileActions.extract(filename, context)
+    });
+this.register('application/x-compressed', 'Extract', OC.PERMISSION_READ, '',
+    function(filename, context) {
+        context.fileActions.extract(filename, context)
+    });
