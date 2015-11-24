011294e9275cdedd1278ca498dffd9b6f6fe1d65
Lars Næsbye Christensen <lars@naesbye.dk>
2015-11-24 11:23:34 +0100

    Automatic refresh upon extraction, better expand function name, licenseplates

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,19 +1,28 @@
-<?php
-/**
- * ownCloud - files_compress
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Lars Næsbye Christensen (lars.christensen@deic.dk)
- * 
- */
+/*                                                                                                                                
+ * files_compress, ownCloud file decompression app 
+ *                                                                                                                                 
+ * This library is free software; you can redistribute it and/or                                                                    
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE                                                               
+ * License as published by the Free Software Foundation; either                                                                     
+ * version 3 of the License, or any later version.                                                                                  
+ *                                                                                                                                  
+ * This library is distributed in the hope that it will be useful,                                                                  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                   
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                    
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.                                                                              
+ *                                                                                                                                  
+ * You should have received a copy of the GNU Lesser General Public                                                                 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.                                                    
+ *                                                                                                                                  
+ */  
 
+<?php
 
 // Include our class of library functions
 OC::$CLASSPATH['OC_Files_Archive_Util'] ='apps/files_compress/lib/archiveutils.php';
 
+// Load our action script
 	if(\OCP\User::isLoggedIn() ){
-		\OCP\Util::addScript('files_compress', 'fileactions'); // load our actions script
+		\OCP\Util::addScript('files_compress', 'fileactions'); 
 }
 
--- js/fileactions.js
+++ js/fileactions.js
@@ -1,4 +1,22 @@
-        function expand (filename, context) {
+/*                                                                                                                                
+ * files_compress, ownCloud file decompression app 
+ *                                                                                                                                 
+ * This library is free software; you can redistribute it and/or                                                                    
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE                                                               
+ * License as published by the Free Software Foundation; either                                                                     
+ * version 3 of the License, or any later version.                                                                                  
+ *                                                                                                                                  
+ * This library is distributed in the hope that it will be useful,                                                                  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                   
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                    
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.                                                                              
+ *                                                                                                                                  
+ * You should have received a copy of the GNU Lesser General Public                                                                 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.                                                    
+ *                                                                                                                                  
+ */  
+
+        function files_mv_expand (filename, context) {
             var dir = context.dir || context.fileList.getCurrentDirectory();
             $.ajax(OC.linkTo('files_compress', 'ajax/extract.php'), {
                 type: 'POST',
@@ -9,13 +27,15 @@
                 dataType: 'json',
                 success: function(s) {
                     if (s.status == "success") { 
-                        alert('Extraction was successful.');
+                       // alert('Extraction was successful.');
                     } else {
                         alert('Could not extract.');
                     }
+                    location.reload();
                 },
                 error: function(s) {
                     alert('An error occurred.');
+                    location.reload();
                 },
 	});
 }
@@ -25,19 +45,19 @@ $(document).ready(function() {
 
         FileActions.register('application/zip', 'Extract', OC.PERMISSION_READ, '',
             function(filename, context) {
-                expand(filename, context)
+                files_mv_expand(filename, context)
             });
         FileActions.register('application/x-gzip', 'Extract', OC.PERMISSION_READ, '',
             function(filename, context) {
-                expand(filename, context)
+                files_mv_expand(filename, context)
             });
         FileActions.register('application/x-rar-compressed', 'Extract', OC.PERMISSION_READ, '',
             function(filename, context) {
-                expand(filename, context)
+                files_mv_expand(filename, context)
             });
         FileActions.register('application/x-compressed', 'Extract', OC.PERMISSION_READ, '',
             function(filename, context) {
-                expand(filename, context)
+                files_mv_expand(filename, context)
             });
     }
 });
