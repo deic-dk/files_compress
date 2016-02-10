41971652b6f5d3a074707f686538d460aa55dce4
Lars Næsbye Christensen <lars@naesbye.dk>
2016-02-10 14:18:04 +0100

    buttonon top bar

--- js/fileactions.js
+++ js/fileactions.js
@@ -1,21 +1,3 @@
-/*                                                                                                                                
- * files_compress, ownCloud archive handling app 
- *                                                                                                                                 
- * This library is free software; you can redistribute it and/or                                                                    
- * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE                                                               
- * License as published by the Free Software Foundation; either                                                                     
- * version 3 of the License, or any later version.                                                                                  
- *                                                                                                                                  
- * This library is distributed in the hope that it will be useful,                                                                  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                   
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                    
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.                                                                              
- *                                                                                                                                  
- * You should have received a copy of the GNU Lesser General Public                                                                 
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.                                                    
- *                                                                                                                                  
- */  
-
         function files_compress_expand (filename, context) {
             var dir = context.dir || context.fileList.getCurrentDirectory();
             $.ajax(OC.linkTo('files_compress', 'ajax/extract.php'), {
@@ -83,5 +65,10 @@ $(document).ready(function() {
             });
             
     }
+        // Add action to top bar (visible when files are selected)
+        $('#app-content-files #headerName .selectedActions').prepend(
+                        '<a class="tag btn btn-xs btn-default" id="tag" href=""><i class="icon icon-compress"></i>'+t('files_compress',' Compress')+'</a>&nbsp;');
+        //$('#headerName .selectedActions .tag').click(OCA.Meta_data.App.tagMultipleDropdown);
+
 });
 
