824c0411e4f43359918f8903470fdb65bb7249b8
Kasper Sort <kasper.sort@deic.dk>
2017-01-20 14:32:32 +0100

    Extract version numbering
    Extract.php now versions decompressed archives. If an archive exists it will be numbered like this: 
    <filename> (versionnumber).<extension>
    This applies to files and directories.

--- ajax/extract.php
+++ ajax/extract.php
@@ -43,17 +43,20 @@ if (OCP\App::isEnabled('files_compress')) {
 	$success = FALSE;
 	$buffer_size = 8192; // buffer for extraction
 
-	// find tar.gz file
+	// for debugging - remove
+	$sti = "/tank/data/tmp";
+	$fh = fopen($sti."/compress.log", 'a');
 
+	// find tar.gz file
 	$pattern  = '/\.(tgz$)|(tar\.gz$)/i';
 	preg_match($pattern, $filename,$match);
 
 	foreach ($match as $key => $value) {
 		if ($value == 'tar.gz') {
 			$mime = 'tar.gz';
-			# code...
 		}
 	}
+
 	if ($mime === 'zip' || $mime === 'gz' || $mime === 'tgz' || $mime === 'tar' || $mime === 'bz2' || $mime = 'tar.gz') {
 
 			// we should do our dirty work in a tempdir
@@ -66,19 +69,20 @@ if (OCP\App::isEnabled('files_compress')) {
 				case 'zip':
 					$unzip = new ZipArchive();
 					
-					// check if we can open archive
+					$b64_tmpfile = base64_encode($filenameWOext);
 					$ziparchive =  $archive_dir . $filename;
 
+					// check if we can open archive
 					/* open archive */
 					if ($unzip->open($ziparchive) === TRUE) {
 						// unzip if archive is open
-						$success = $unzip->extractTo($temp_dir);        
+						$success = $unzip->extractTo($temp_dir);
 					} else {
 						$success = FALSE;
 					}
-
 					$unzip->close();
 					break;
+
 				case 'tar.gz':
 					
 					$cmd = escapeshellcmd('/usr/bin/tar -xzf '.escapeshellarg($ziparchive).' -C '.escapeshellarg($temp_dir));
@@ -126,8 +130,6 @@ if (OCP\App::isEnabled('files_compress')) {
 					$p = new PharData($ziparchive);
 					$p->decompress(); // creates /path/to/my.tar
 					*/
-					$tid2 = time();
-					$tid = $tid2-$tid1;
 					break; 
 				case 'bz2':
 					$bz = bzopen($ziparchive, "r") or die("Couldn't open $ziparchive for reading");
@@ -165,37 +167,79 @@ if (OCP\App::isEnabled('files_compress')) {
 					# code...
 					break;
 			}
+			$tid2 = time();
+			$tid = $tid2-$tid1;
+
+
+
+			// Findes filen eller noget der ligner
+			
+			$dirlist = array();
+			$dirlist = scandir($temp_dir);
+			$versioning = array();
+			$matchfile  = array();
+			$versionNumber = 0;
+			
+			foreach ($dirlist as $key => $file) {
+					if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) ) {
+						continue;
+					}
+				//$pattern  = '/^('.$file_parts[0].')/i';
+				//if (preg_match($pattern, $value, $m)) {
+					//fwrite($fh, "FILE: $value\n\n");
+					//$dir = rtrim($archive_dir,'/');
+					$target = OCP\Files::buildNotExistingFileName(stripslashes($dir), $file);
+					//fwrite($fh, "TARGET: $file::$target\n".stripslashes($dir)."\n");
+					OCP\Util::writeLog("files_compress", "B: $target::$file::$dir", \OC_Log::ERROR);
+					//array_push($matchfile, $value);
+				//}
+			}
+
 
 
 			/* Move the files to correct path */
 			if (is_dir($temp_file) === true) {
+				$target = OCP\Files::buildNotExistingFileName(stripslashes($dir), $temp_file);
+				fwrite($fh, "FILE: ".$temp_file."\nNEW FILE: $target\n######\n");
 				$mvcmd = escapeshellcmd("mv ".escapeshellarg($temp_file).' '.escapeshellarg($archive_dir));
+
+				$mvfiles = 1;
 			} else {
 				$extract_dir = rtrim($extract_dir, "/");
+				$mvfiles = 1;
 			}
 
 
 			// cleanup by deleting all files and temp directory
-			$files = glob($temp_dir . '{,.}*', GLOB_BRACE);
-			if (is_file($temp_file)) {
-			
+			//$files = glob($temp_dir . '{,.}*', GLOB_BRACE);
+			$files = scandir($temp_dir);
+
+			if ($mvfiles === 1) {
+
 				foreach ($files as $file) { // iterate files
+					// Skip '.' and '..'
 					if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) ) {
 						continue;
 					}
-						
+					$dir = rtrim($dir,'/');
+					$archive_dir = rtrim($archive_dir,'/');
+					$target = OCP\Files::buildNotExistingFileName(stripslashes($dir), $file);
+
+					//$mvcmd = escapeshellcmd("mv ".escapeshellarg($file).' '.escapeshellarg($archive_dir.stripslashes($target)));
+					$newdest = $archive_dir.stripslashes($target);
+					fwrite($fh, "FILE: ".$file."\nNEW FILE: $newdest\n######\n");
+
+					rename($temp_dir.$file, $newdest);
+					/*if (exec($mvcmd)) {
+						$success = TRUE;
+					}
+*/
 					if (is_file($file)) {
 						//unlink($file);
 					} else { // delete file
 						$p2 = '/(\.)|(\.\.)/';
 					}
-
-					$mvcmd = escapeshellcmd("mv ".escapeshellarg($file).' '.escapeshellarg($archive_dir));
-					
-					if (exec($mvcmd)) {
-						$success = TRUE;
-					}
-
+				
 				}
 			} else {
 				if (exec($mvcmd)) {
@@ -203,7 +247,7 @@ if (OCP\App::isEnabled('files_compress')) {
 				}
 			}
 	
-		$success = file_exists($newdest);
+		// $success = file_exists($newdest);
 
 		// cleanup by deleting temp directory
 		if (file_exists($temp_dir)) {
